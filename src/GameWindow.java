2068b4d6aceaf93c9ada3777ed584661d858f134
Will Taylor <w1ll100@hotmail.co.uk>
2017-07-18 18:14:38 +0100

    Fixed (hopefully) content changing

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -4,25 +4,51 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GameWindow extends JFrame{
+public class GameWindow extends JFrame implements ActionListener{
 	private Container content;
+	private static JButton startButton = new JButton("Start");
+
 	public GameWindow(){
 		super("As yet unnamed bullet hell game");
-		content = getContentPane();
-		content.setLayout(new BorderLayout());
 		setSize(640, 480);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		content = getContentPane();
+		content.setLayout(new BorderLayout());
+
+		JPanel jp = new JPanel();
+		startButton.addActionListener(this);
+		jp.add(startButton);
+
+		content.add(jp, BorderLayout.CENTER);
+
 		setVisible(true);
 	}
 
-	public void newGamePlay(GamePlay gp){
-		content.add(gp, BorderLayout.CENTER);
+	@Override
+	public void actionPerformed(ActionEvent e){
+		Object actionSource = e.getSource();
+
+		if(actionSource == startButton){
+			newGamePlay();
+		}
+	}
+
+	public void newGamePlay(){
+		GamePlay gp = new GamePlay(this, new GameLevel());
+		gp.setFocusable(true);
+		changeContentPane((JPanel) gp);
+	}
+
+	public void changeContentPane(JPanel newPanel){
+		JPanel jp = (JPanel) content;
+		jp.removeAll();
+		jp.add(newPanel);
+		jp.revalidate();
+		//jp.repaint();		Not sure if needed - seems to work without
 	}
 
 	public static void main(String[] args){
 		GameWindow window = new GameWindow();
-		GamePlay gp = new GamePlay(window, new GameLevel());
-		gp.setFocusable(true);
-		window.newGamePlay(gp);
-	}	
+	}
 }
