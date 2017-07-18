fa7d837302e5f635aef17b9978516da4a0a9865e
Will Taylor <w1ll100@hotmail.co.uk>
2017-07-18 18:15:21 +0100

    Player object created

--- src/GamePlayLogic.java
+++ src/GamePlayLogic.java
@@ -1,14 +1,18 @@
 package src;
 
+import src.gameobjects.*;
+
 public class GamePlayLogic{
 	private GameLevel level;
+	private Player player;
 
 	public GamePlayLogic(GameLevel level){
 		this.level = level;
+		player = new Player(300, 500);
 		System.out.println("level loaded");
 	}
 
 	public void update(long gameTime){
-
+		System.out.println(String.format("TICK %d", gameTime));
 	}
 }
--- src/gameobjects/Player.java
+++ src/gameobjects/Player.java
@@ -1,5 +1,8 @@
 package src.gameobjects;
 
 public class Player extends Ship{
-
+	public Player(int x, int y){
+		moveTo(x, y);
+		System.out.println(String.format("Player pos: %d %d", getX(), getY()));
+	}
 }
