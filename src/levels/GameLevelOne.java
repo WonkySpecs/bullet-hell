60095ec5eb0599c970907846263324f0fb72703b
Will Taylor <w1ll100@hotmail.co.uk>
2017-07-26 18:54:29 +0100

    GameLevelOne extended for more example gameplay

--- src/levels/GameLevelOne.java
+++ src/levels/GameLevelOne.java
@@ -24,13 +24,25 @@ public class GameLevelOne extends GameLevel{
 
 	@Override
 	public ArrayList<EnemyShip> getEnemySpawns(long gameTime){
-		if(gameTime == 50){
+		HashMap<String, Animation> floaterAnimations = AnimationMapFactory.getAnimationMap("enemyfloater1");
+
+		if(gameTime == 35){
 			ArrayList<EnemyShip> floaters = new ArrayList<>();
 
-			HashMap<String, Animation> floaterAnimations = AnimationMapFactory.getAnimationMap("enemyfloater1");
-			floaters.add(new EnemyStraightFloater(20, -30, 100, 2, 7, floaterAnimations));
-			floaters.add(new EnemyStraightFloater(screenWidth - 20, -30, 100, -2, 7, floaterAnimations));
+			floaters.add(new EnemyStraightFloater(50, -30, 100, 2, 7, floaterAnimations));
+			floaters.add(new EnemyStraightFloater(getScreenWidth() - 66, -30, 100, -2, 7, floaterAnimations));
 			return floaters;
+		}
+
+		if(gameTime > 110 && gameTime < 190){
+			if(gameTime % 12 == 0){
+				ArrayList<EnemyShip> floaters = new ArrayList<>();
+				floaters.add(new EnemyStraightFloater(getScreenWidth()/2, -30, 100, 0, 4, floaterAnimations));
+				floaters.add(new EnemyStraightFloater(-20, getScreenHeight()/2, 100, 3, -2, floaterAnimations));
+				floaters.add(new EnemyStraightFloater(getScreenWidth() + 20, getScreenHeight()/2, 100, -3, -2, floaterAnimations));
+
+				return floaters;
+			}
 		}
 		return null;
 	}
