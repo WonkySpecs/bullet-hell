a4adc3ab7dbdfbef9830d9e031e18d066bca7cc2
Will Taylor <w1ll100@hotmail.co.uk>
2017-08-06 18:05:48 +0100

    Completed first iteration of ItemDrop class

--- src/gameobjects/ItemDrop.java
+++ src/gameobjects/ItemDrop.java
@@ -5,6 +5,8 @@
 * death.
 * Once the player comes within a certain radius of the item
 * it will move towards the player.
+* GamePlayLogic should call checkIfTracking(playerHitbox)
+* then update (screenWidth, screenHeight)
 *
 * @author  Will Taylor
 * @version 0.1
@@ -15,25 +17,71 @@ package src.gameobjects;
 
 import java.util.HashMap;
 
-import src.GameObject;
-import src.Hitbox;
+import src.*;
 import src.animation.*;
 
 public class ItemDrop extends GameObject{
 	private static double DROP_SPEED = 4;
+	private static double DEFAULT_TRACKING_RANGE = 20;
+	private boolean tracking;
+	private double trackingRange;
 
-	public ItemDrop(double x, double y, Hitbox hitbox, HashMap<String, Animation> animations){
+	public ItemDrop(double x, double y, double trackingRange, Hitbox hitbox, HashMap<String, Animation> animations){
 		super(x, y, hitbox, animations);
 		setXvel(DROP_SPEED);
+		setYvel(0);
+		tracking = false;
+		this.trackingRange = trackingRange;
 	}
 
+	public ItemDrop(double x, double y, Hitbox hitbox, HashMap<String, Animation> animations){
+		this(x, y, DEFAULT_TRACKING_RANGE, hitbox, animations);
+	}
+
+	//Before getting close to the player, move straight down
+	//Once tracking range has been entered, move towards player
 	@Override
 	public void update(int screenWidth, int screenHeight){
 		super.update(screenWidth, screenHeight);
 		moveDown();
+		moveRight();
 
 		//if(!isRemovable() && !isOffScreen(screenWidth, screenHeight)){
 		//	setRemovable(true);
 		//}
+	}
+
+	public void track(HitboxCircle playerHitbox){
+		//Speed decreases exponentially with respect to distance
+		// = DROP_SPEED at DEFAULT_TRACKING_RANGE
+		if(!tracking){
+			checkIfTracking(playerHitbox)
+		}
+
+		if(tracking){
+			double dx = playerHitbox.getCenter().getX() - getX();
+			double dy =  playerHitbox.getCenter().getY() - getY();
+			double targetAngle = Math.atan2(dy, dx);
+			double dist = Math.sqrt(dx * dx + dy * dy);
+
+			double speed;
+			//If item is very close, don't want to move past. Also solves any
+			//possibility of div by 0 errors
+			if(dist < 2){
+				speed = 1;
+			}
+			else{
+				speed = DROP_SPEED * DEFAULT_TRACKING_RANGE / dist;
+			}
+			setXvel(speed * Math.cos(targetAngle));
+			setYvel(speed * Math.sin(targetAngle));			
+		}
+	}
+
+	public void checkIfTracking(HitboxCircle playerHitbox){
+		if(playerHitbox.getCenter().distance(getHitbox().getCenter()) > trackingRange){
+			System.out.println("tracking");
+			tracking = true;
+		}
 	}
 }
